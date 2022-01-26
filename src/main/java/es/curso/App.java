f8c4cf2476a1fe11b7b7ad37ea86a7bfe1710333
Ubuntu <ubuntu@ip-172-31-5-147.eu-west-1.compute.internal>
2022-01-26 12:25:22 +0000

    Introducido el bug

--- src/main/java/es/curso/App.java
+++ src/main/java/es/curso/App.java
@@ -13,7 +13,7 @@ public class App
     
     public static int doblar( int numero )
     {
-        return numero*2;
+        return numero*3;
     }
     
     public static int mayor( int numero1, int numero2 )
